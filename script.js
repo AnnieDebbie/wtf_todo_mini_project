5366ef467657973368b02ee95869161418d67fb6
Deborah Animashaun <deborahanimashaun659@yahoo.com>
2022-10-19 17:47:45 +0100

    Feat: Style todo list and Add form validation

--- script.js
+++ script.js
@@ -3,18 +3,25 @@ const todoList = document.querySelector(".todo-list");
 const form = document.querySelector(".add-todo-form");
 let addBtn = document.getElementById("add-todo-btn");
 
-function createListItem(enteredTodo){
-    let listItem = document.createElement("li");
-    listItem.innerText = enteredTodo;
-    listItem.classList.add('todo-item')
+function createListItem(enteredTodo) {
+  let listItem = document.createElement("li");
+  listItem.innerText = enteredTodo;
+  listItem.classList.add("todo-item");
+  todoList.prepend(listItem);
+}
+
+function renderError() {
+  const errorDiv = `
+  <div class=errorDiv> <p class="error-message"> Invalid input, please put in a Todo </p> </div>
+  `;
+  todoList.insertAdjacentHTML("afterbegin", errorDiv);
 }
 
 function submitHandler(e) {
   e.preventDefault();
   let enteredTodo = addTodoInput.value;
-  createListItem(enteredTodo)
-  todoList.prepend(listItem);
+  createListItem(enteredTodo);
+  enteredTodo.length <= 0 && renderError();
 }
 
 form.addEventListener("submit", submitHandler);
-
